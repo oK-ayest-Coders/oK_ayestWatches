19ad7fb87d3529956cbe7b84bf723a3e6b7d1a4c
Lucas Woods <lukewoods@Lucass-MacBook-Pro.local>
2024-01-02 15:46:44 -0600

    updated changes to routing

--- src/server/api/watches.js
+++ src/server/api/watches.js
@@ -1,4 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../client")
+
+router.get("/", async (req, res, next) => {
+    try{
+        const watches = await prisma.watches.findMany();
+        res.status(200).send(watches)
+    } catch(error){
+        console.error(error)
+
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    const {id} = req.params;
+    try{
+        const watch = await prisma.watches.findUnique({
+            where: { 
+               id: +id
+            },
+        });
+        res.status(200).send(watch);
+    } catch(error){
+        console.error(error);
+    }
+});
 
 module.exports = router;
